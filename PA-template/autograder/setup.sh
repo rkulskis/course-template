447217f84b0ca8ab8f3e079b83d06663e8f42956
rkulskis <rkulskis@bu.edu>
2024-12-08 22:43:35 -0500

    feat: now autograder/ is autogenerated
    You can change the config in your Makefile but this makes the code footprint
    smaller and concise to only what you need.

--- PA-template/autograder/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get -y install openjdk-11-jdk
-apt-get install -y python3 python3-pip python3-dev
-
-pip3 install -r /autograder/source/requirements.txt
-# just to be VERY safe we didn't add sym link to solution/submission.py
-rm -rf /autograder/source/submission.py
-rm -rf /autograder/source/tests/submission.py
